14dbb3a8db03dd426adffd81e5c1aa77d03fc1da
Katya <katya@ddd.ggg>
2022-10-20 06:21:31 +0000

    63.5

---
+++ cycles/63.5.php
@@ -0,0 +1,20 @@
+<?php
+	$arr = [
+		1 => 125,
+		2 => 225,
+		3 => 128,
+		4 => 356,
+		5 => 145,
+		6 => 281,
+		7 => 452,
+	];
+    $arr_fin = [];
+    foreach ($arr as $key => $elem){
+        $str = (string)$elem;
+        if($str[0] == '1' or $str[0] == '2'){
+            $arr_fin[] = $elem;
+
+        }
+    }
+    var_dump($arr_fin);
+?>
