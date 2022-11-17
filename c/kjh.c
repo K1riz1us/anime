1cc93082cb462df622ec08ad7ed36f82e21f6e42
Katya <katya@ddd.ggg>
2022-11-17 06:10:02 +0000

    17.11

---
+++ c/kjh.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+    char tracks[][80]={
+        "Tigers eye",
+        "Antoshka",
+        "Peremen",
+        "Podozritelno",
+        "Kukuhka"
+    };
+    void find_track(char search_for[]){
+    for(int i=0; i<5; i++){
+        if(strstr(tracks[i], search_for)){
+            printf("Pesnya nomer: %d: %s" , i , tracks[i]);
+        }
+    }
+}
+int main(){
+    char search_for[80];
+    printf("Искать: ");
+    fgets(search_for, 80, stdin);
+    search_for[strlen(search_for)-1]= '\0';
+    find_track(search_for);
+    return 0;
+}
